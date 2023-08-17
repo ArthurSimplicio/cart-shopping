79d25b55bec5b724caabeca84f9a9365c8a71bce
ArthurSimplicio <arthurpark20@gmail.com>
2023-08-17 15:59:37 -0300

    correcoes projeto

--- src/app/api/fetchProducts.tsx
+++
@@ -1,7 +0,0 @@
-
-const fetchProducts = async (query:string|number) => {
-    const res = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
-    const data = await res.json();
-    return data.results;
-}
-export default fetchProducts
